ff789c27dbb9f89c92d18c71734b53e688aaca76
Mohammed Nasif <m.khaled100@ymail.com>
2025-08-02 16:42:56 +0300

    feat: restructure App component to implement routing with AuthRedirect and ProtectedRoute

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,54 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Home from './pages/Home';
+import ProtectedRoute from './components/ProtectedRoute';
+import AuthRedirect from './components/AuthRedirect';
 
+function App() {
   return (
-    <>
-    Welcome
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Public Routes - Redirect authenticated users to home */}
+        <Route 
+          path="/signup" 
+          element={
+            <AuthRedirect redirectTo="/home">
+              <Signup />
+            </AuthRedirect>
+          } 
+        />
+        <Route 
+          path="/signin" 
+          element={
+            <AuthRedirect redirectTo="/home">
+              <Signin />
+            </AuthRedirect>
+          } 
+        />
+        
+        {/* Protected Route - Only for authenticated users */}
+        <Route 
+          path="/home" 
+          element={
+            <ProtectedRoute 
+              redirectTo="/signin"
+              loadingMessage="Loading your dashboard..."
+              showUserInfo={import.meta.env.DEV}
+            >
+              <Home />
+            </ProtectedRoute>
+          } 
+        />
+        
+        {/* Default redirect */}
+        <Route path="/" element={<Navigate to="/signin" replace />} />
+        
+        {/* Catch all */}
+        <Route path="*" element={<Navigate to="/signin" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
